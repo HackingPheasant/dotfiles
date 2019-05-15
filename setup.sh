d30e3d22e0c422d28aa9a4f8c68202c8d832697d
HackingPheasant <HackingPheasant@protonmail.com>
2019-05-15 17:41:22 +1000

    Add setup script to install preferred apps on solus

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+sudo eopkg update
+sudo eopkg install -c system.devel
+sudo eopkg iistall rclone mpv git vim vlc deadbeef beets stow discord riot hub ncdu p7zip ruby ruby-devel
+sudo pip3 install internetarchive buku flask pylast
+sudo gem install bundler jekylll
