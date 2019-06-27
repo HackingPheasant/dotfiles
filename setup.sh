57d49a4ff350ebbb840280adff1bc47e32f33538
HackingPheasant <HackingPheasant@protonmail.com>
2019-06-27 15:32:48 +1000

    setup.sh: updated
    I currently dont like how it is, at the moment its more of a scratch pad
    for me to remeber what to install on a new machine

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 sudo eopkg update
 sudo eopkg install -c system.devel
-sudo eopkg iistall rclone mpv git vim vlc deadbeef beets stow discord riot hub ncdu p7zip ruby ruby-devel
+sudo eopkg iistall rclone mpv git vim vlc deadbeef beets stow discord riot hub ncdu p7zip ruby ruby-devel libvirt virt-manager
 sudo pip3 install internetarchive buku flask pylast
-sudo gem install bundler jekylll
+sudo gem install bundler jekyll
+sudo systemctl enable libvirtd.service
+sudo systemctl start libvirtd.service
