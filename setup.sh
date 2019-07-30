c15412a006a72eb2855f8df45abb2bd396518e6c
HackingPheasant <HackingPheasant@protonmail.com>
2019-07-30 13:26:42 +1000

    setup.sh: clean up

--- setup.sh
+++ setup.sh
@@ -1,9 +1,26 @@
 #!/usr/bin/bash
 
+mkdir -p $HOME/.local/{bin,lib,share}
+
+# Update Default system packages
 sudo eopkg update
+
+# Install Build Tools
 sudo eopkg install -c system.devel
-sudo eopkg iistall rclone mpv git vim vlc deadbeef beets stow discord riot hub ncdu p7zip ruby ruby-devel libvirt virt-manager
+
+# Install some programs I find useful
+sudo eopkg install git stow rclone mpv vim vlc beets discord riot hub ncdu p7zip
 sudo pip3 install internetarchive buku flask pylast
+
+# Tools for GitHub Pages Blog
+sudo eopkg install ruby ruby-devel
 sudo gem install bundler jekyll
+
+# Virtualisation
+sudo eopkg install libvirt virt-manager
 sudo systemctl enable libvirtd.service
 sudo systemctl start libvirtd.service
+
+# TODO: 
+# - Stow my stuff automatically
+# - Have a more modular setup.sh that work on multiple distros
