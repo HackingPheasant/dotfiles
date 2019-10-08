HackingPheasant/dotfiles

commit 86663b77bd2ef98eb4558a5f02be0cf9a754d9a9

terminal: add gem path and general cleanup

bash/.bashrc
@@ -1,12 +1,6 @@
 # ~/.bashrc - bash interactive startup file
 # vim: ft=sh
 
-if [[ $(lsb_release -i) == *Solus* ]]; then
-	# Source solus stateless config
-	# the stateless concept - a strict separation between User and System files for easier OS manageability.
-	source /usr/share/defaults/etc/profile
-fi
-
 have() { command -v "$1" >&/dev/null; }
 
 if [[ ! $PREFIX ]]; then

sh/.environ
@@ -28,12 +28,13 @@ setpath PATH \
 	"$PREFIX/bin"				\
 	"$GOPATH/bin"				\
 	"$HOME/.scripts"			\
-	"$PATH"						;
+        "$PREFIX/gems/bin"                      \
+	"$PATH"					;
 
+GOPATH="$PREFIX/go"
 DOTNET_ROOT="$PREFIX/lib/dotnet"
-
-# Cisco Packet Tracer
-PT7HOME=/opt/pt
+GEM_HOME="$PREFIX/lib/gems"
+PT7HOME="/opt/pt" # Cisco Packet Tracer
 
 # http://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html
 
@@ -44,7 +45,7 @@ test "$XDG_RUNTIME_DIR" || XDG_RUNTIME_DIR="$XDG_CACHE_HOME"
 
 
 # identity, locale
-# Prefer AU English and use UTF-8
+# Prefer English and use UTF-8
 LANG="en_US.UTF-8";
 LC_ALL="en_US.UTF-8";
 

commit b20d91a0c5b1c322cf5ddca36feb97232f89a03e

terminal: fix breakage caused by commit 86663b77bd
When I initally removed the duplicate code that was shared between
.bashrc and .profilem I didn't test much. What I realised was that
display managers weren't sourcing .profile, which had the code to
source the system level modifications, this broke stuff subtly, like tab
completion stopped working.

I moved the code to from ~/.profile to ~/bashrc to fix the problem.

Note: Why is shell file loading order so damn complicated.

bash/.bashrc
@@ -1,6 +1,12 @@
 # ~/.bashrc - bash interactive startup file
 # vim: ft=sh
 
+if [[ $(lsb_release -i) == *Solus* ]]; then
+        # Source solus stateless config
+        # the stateless concept - a strict separation between User and System files for easier OS manageability.
+        source /usr/share/defaults/etc/profile
+fi
+
 have() { command -v "$1" >&/dev/null; }
 
 if [[ ! $PREFIX ]]; then

sh/.profile
@@ -1,12 +1,6 @@
 # ~/.profile - sh/bash login script
 # vim: ft=sh
 
-if [[ $(lsb_release -i) == *Solus* ]]; then
-	# Source solus stateless config
-	# the stateless concept - a strict separation between User and System files for easier OS manageability.
-	source /usr/share/defaults/etc/profile
-fi
-
 . ~/.environ
 
 have() { type "$1" >/dev/null 2>&1; }