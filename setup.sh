fee4c3afc0f5b78cc7f331fa24d0484705d26052
HackingPheasant <HackingPheasant@protonmail.com>
2019-10-08 11:46:16 +1100

    setup.sh: Update script

--- setup.sh
+++ setup.sh
@@ -3,19 +3,19 @@
 mkdir -p $HOME/.local/{bin,lib,go,share}
 
 # Update Default system packages
-sudo eopkg update
+sudo eopkg upgrade
 
 # Install Build Tools
 sudo eopkg install -c system.devel
 
 # Install some programs I find useful
 sudo eopkg rm gnome-mpv thunderbird transmission rhythmbox
-sudo eopkg install git stow rclone mpv vim vlc beets discord riot ncdu p7zip deluge mkvtoolnix mediainfo gtkhash gtkhash-nautilus-extension
+sudo eopkg install git stow rclone kitty mpv vim vlc beets keyutils fzf jq discord riot ncdu p7zip deluge mkvtoolnix mediainfo gtkhash gtkhash-nautilus-extension
 sudo pip3 install internetarchive pylast
-sudo pip3 install git+git://github.com/geigerzaehler/beets-check.git@master
+# sudo pip3 install git+git://github.com/geigerzaehler/beets-check.git@master
 # Dependicies for buku and bukuserver
 # Probley best to do this inside a virtual env but eh...
-sudo pip3 install buku arrow flask flask_admin flask_api flask_bootstrap flask_paginate flask_wtf 
+sudo pip3 install buku arrow flask flask_admin flask_api flask_bootstrap flask_paginate flask_wtf
 # Tools for GitHub Pages Blog
 sudo eopkg install ruby ruby-devel
 sudo gem install bundler jekyll
@@ -25,6 +25,8 @@ sudo eopkg install libvirt virt-manager
 sudo systemctl enable libvirtd.service
 sudo systemctl start libvirtd.service
 
-# TODO: 
+# TODO:
 # - Stow my stuff automatically
 # - Have a more modular setup.sh that work on multiple distros
+# - Modify x-terminal-emulator 
+# e.g. sudo ln -sf /usr/bin/kitty /usr/bin/x-terminal-emulator
