9ce4afeb9b44f87ca2dc22966931796286ebc853
HackingPheasant <HackingPheasant@protonmail.com>
2020-01-15 23:19:00 +1100

    scripts: add purge wine associations script
    I find myself doing this often enough that I thouht I might script it!

---
+++ scripts/.scripts/purge-wine-assoc.sh
@@ -0,0 +1,7 @@
+rm -f ~/.local/share/applications/wine-extension*.desktop
+rm -f ~/.local/share/icons/hicolor/*/*/application-x-wine-extension*
+rm -f ~/.local/share/applications/mimeinfo.cache
+rm -f ~/.local/share/mime/packages/x-wine*
+rm -f ~/.local/share/mime/application/x-wine-extension*
+update-desktop-database ~/.local/share/applications
+update-mime-database ~/.local/share/mime/
