73c221c5ec6cdb949c5b1ef72f518848818d4bc3
HackingPheasant <HackingPheasant@protonmail.com>
2020-10-12 23:28:07 +1100

    awesomewm: cleanup user created bindings

--- awesomewm/.config/awesome/bindings/global_usercreated_keys.lua
+++ awesomewm/.config/awesome/bindings/global_usercreated_keys.lua
@@ -13,23 +13,17 @@ local shiftkey = common.shiftkey
 
 -- My Customisations 
 awful.keyboard.append_global_keybindings({ 
+    -- miscellaneous
     awful.key({ modkey,           }, "e", function () awful.spawn(common.filemanager) end,
               {description = "open a filemanager", group = "launcher"}),
 
     -- screenshots
-    -- TODO: Decide on non-conflicting keys to use
     awful.key({                   }, "Print", function () screenshot.action("full") end,
               {description = "Screenshot entire screen", group = "Screenshots"}),
     awful.key({ altkey,           }, "Print", function () screenshot.action("selection") end,
               {description = "Screenshot selected area", group = "Screenshots"}),
     awful.key({ modkey,           }, "Print", function () screenshot.action("window") end,
               {description = "Screenshot current window", group = "Screenshots"}),
-    --awful.key({ altkey, "v"       }, "Print", function () screenshot.action("clipboard") end,
-    --          {description = "Screenshot selected area and copy to clipboard", group = "Sceenshots"}),
-    --awful.key({ modkey, "b"       }, "Print", function () screenshot.action("browse") end,
-    --          {description = "Browse Screenshots", group = "Screenshots"}),
-    --awful.key({ modkey, "g"       }, "Print", function () screenshot.action("gimp") end,
-    --          {description = "Edit most recent screenshot with gimp", group = "Screenshots"}),
 
     -- sound
     -- TODO: Move everything below this comment into helper/dedicated file
@@ -41,15 +35,8 @@ awful.keyboard.append_global_keybindings({
               {description = "Lower Volume", group = "Function Keys"}),
     
     -- brightness
-    -- xbacklight currently doesnt work on my laptop
-    -- No outputs have backlight property
-    -- TODO: FIX IT
-    awful.key({                   }, "XF86MonBrightnessUp", function () awful.spawn("xbacklight -inc 5%") end,
+    awful.key({                   }, "XF86MonBrightnessUp", function () awful.spawn("light -A 5%") end,
               {description = "Increase Brightness", group = "Function Keys"}),
-    awful.key({                   }, "XF86MonBrightnessDown", function () awful.spawn("xbacklight -dec 5%") end,
+    awful.key({                   }, "XF86MonBrightnessDown", function () awful.spawn("light -U 5%") end,
               {description = "Decrease Brightness", group = "Function Keys"})
-
-    -- lockscreen
-    -- awful.key({ ctrlkey           }, "l", function() awful.spawn("awesome-client '_G.show_lockscreen()'", false) end,
-    --          {description = "lock the screen", group = "Utility"})
 })
